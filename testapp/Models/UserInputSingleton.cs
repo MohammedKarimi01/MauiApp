c0ddfa5cd11cf25df4578a52344895ef2e1a33d2
Mohammed Karimi <mohammed.karimi01@hotmail.com>
2023-05-28 12:33:10 +0200

    Add project files.

---
+++ testapp/Models/UserInputSingleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testapp.Models
+{
+    public class UserInputSingleton
+    {
+        private static UserInputSingleton instance;
+
+        public string UserInputName { get; set; }
+
+        private UserInputSingleton()
+        {
+            UserInputName = string.Empty;
+        }
+
+        public static UserInputSingleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new UserInputSingleton();
+                }
+
+                return instance;
+            }
+        }
+    }
+
+
+}
---
+++ testapp/Views/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿namespace testapp;
+using Camille.Enums;
+using Camille.RiotGames;
+using RiotSharp;
+using testapp.Models;
+
+public partial class MainPage : ContentPage
+{
+
+    public MainPage()
+    {
+        InitializeComponent();
+
+        UserInputSingleton.Instance.UserInputName = string.Empty;
+
+    }
+    int count = 0;
+
+    private async void OnButtonClicked(object sender, EventArgs e)
+    {
+
+        UserInputSingleton.Instance.UserInputName = summonerNameEntry.Text;
+        string userInputName = UserInputSingleton.Instance.UserInputName;
+
+        var riotApi = RiotGamesApi.NewInstance("RGAPI-658cd94e-089e-405c-95c5-a4ace9707061");
+
+        try
+        {
+            var summoner = await riotApi.SummonerV4().GetBySummonerNameAsync(PlatformRoute.EUW1, userInputName);
+
+            if (summoner == null)
+            {
+                outputLabel.Text = "Summoner not found.";
+                return;
+            }
+
+            string outputText = $"\n {UserInputSingleton.Instance.UserInputName} var skrivet med singleton \n {summoner.Name}'s Top 25 Champions: \n{summoner.Name}'s Current level is: {summoner.SummonerLevel}\n\n Champion name   \t Champion Points & rank\n";
+
+
+            string SingleTonOutPut = "Detta namn : " + UserInputSingleton.Instance.UserInputName + "vart utskrivet med singleton";
+           
+            var masteries = await riotApi.ChampionMasteryV4().GetAllChampionMasteriesAsync(PlatformRoute.EUW1, summoner.Id);
+
+            for (int i = 0; i < 25; i++)
+            {
+                var mastery = masteries[i];
+                var champion = (Champion)mastery.ChampionId;
+                outputText += $"{i + 1}) {champion,-10} \t\t {mastery.ChampionPoints,10} ({mastery.ChampionLevel})\n";
+            }
+
+            outputLabel.Text = outputText;
+        }
+        catch (RiotSharpException ex)
+        {
+            outputLabel.Text = $"Error: {ex.Message}";
+        }
+    }
+}
+
